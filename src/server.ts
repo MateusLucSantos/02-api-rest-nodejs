f6abc6e2cfa2b5151df5a1e80d7b187ac88789b9
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-04-08 17:06:15 -0300

    Iniciando o projeto, configuracoes iniciais

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello World!'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP Server running!')
+})
