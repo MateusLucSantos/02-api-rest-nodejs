82fb792f21839fd7d691943b20a6b75ebef3d5db
MateusLucSantos <mateuslucsantos.dev@gmail.com>
2025-04-25 22:01:17 -0300

    testando a listagem das transações

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,7 +1,8 @@
-import { it, beforeAll, afterAll } from 'vitest'
+import { it, beforeAll, afterAll, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 import { describe } from 'node:test'
+import { title } from 'process'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -12,7 +13,7 @@ describe('Transactions routes', () => {
     await app.close()
   })
 
-  it('user can create a new transaction', async () => {
+  it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
       .send({
@@ -21,5 +22,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Nova transação',
+        amount: 50000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies || [])
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Nova transação',
+        amount: 50000,
+      }),
+    ])
   })
 })
